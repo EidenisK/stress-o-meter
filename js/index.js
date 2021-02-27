c340a242fbcf37f620dcd344d9b964fe2fc0c883
Eidenis Kasperavičius <eidenis.gargzdai@gmail.com>
2021-02-27 20:42:24 +0200

    revert

--- js/index.js
+++ js/index.js
@@ -97,6 +97,7 @@ function handleSignoutClick(event) {
 async function getBaseNumber() {
     doc = await db.collection('users').doc(userId).get();
     if(!doc.exists) {
+        console.log("Creating new base number...");
         setBaseNumber(1);
     } else {
         baseNumber = doc.data().baseNumber;
@@ -108,6 +109,7 @@ async function setBaseNumber(n) {
         baseNumber: n
     });
     baseNumber = n;
+    console.log("Created new base number");
 }
 //----------------------------------------------------------------
 
